ca4626194dcc0a67b525020788bb331ccfdb199b
deadean <deadean@yandex.ru>
2015-07-25 11:56:54 +0300

    #update MyExample

--- MainApp/UI/Common/Views/MainApp.UI.Common.Views/Implementations/Views/Controls/PageToolBars.xaml.cs
+++ MainApp/UI/Common/Views/MainApp.UI.Common.Views/Implementations/Views/Controls/PageToolBars.xaml.cs
@@ -13,12 +13,12 @@ namespace MainApp.UI.Common.Views.Implementations.Views.Controls
 		public PageToolBars()
 		{
 			InitializeComponent();
-			//if (Device.OS == TargetPlatform.Windows)
-			//{
-			//	toolbar1.Icon = (FileImageSource)ImageSource.FromFile("Assets\\edit.png");
-			//	//toolbar2.Icon = (FileImageSource)ImageSource.FromFile("Assets\\feature.search.png");
-			//	//toolbar3.Icon = (FileImageSource)ImageSource.FromFile("Assets\\refresh.png");
-			//}
+			if (Device.OS == TargetPlatform.Windows)
+			{
+				toolbar1.Icon = (FileImageSource)ImageSource.FromFile("Assets\\edit.png");
+				toolbar2.Icon = (FileImageSource)ImageSource.FromFile("Assets\\feature.search.png");
+				toolbar3.Icon = (FileImageSource)ImageSource.FromFile("Assets\\refresh.png");
+			}
 		}
 
 		public void OnToolbarItemClicked(object sender, EventArgs args)
