403058b64e18902dd2ec5ea84b5ad65435ef97d4
Oleksandr Smyk <oleksandr@ASmyk.local>
2016-02-05 12:16:28 +0200

    #updated ezaudiobindings

--- EZAudioBindings/BindingSample/src/binding/AssemblyInfo.cs
+++ EZAudioBindings/BindingSample/src/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libXMBindingLibrarySampleUniversal.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator64, Frameworks = "QuartzCore UIKit Foundation", LinkerFlags="-lxml2 -ObjC", ForceLoad = true)]
+[assembly: LinkWith ("libXMBindingLibrarySampleUniversal.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator64, Frameworks = "QuartzCore UIKit Foundation Accelerate", LinkerFlags="-lxml2 -ObjC", ForceLoad = true)]
